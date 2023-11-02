beb3e3ae49420663040bf6b0449e615e293d9ee1
Guhfrontend <138324368+Guhfrontend@users.noreply.github.com>
2023-11-02 18:38:32 -0300

    java puro

---
+++ projetos/Linguagem Java/Java1.java
@@ -0,0 +1,29 @@
+
+
+public class Java1 {
+
+    public static void main(String[] args){
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: top Gun: Maverick");
+
+        //Top Gun: Maverick
+        int anoDeLancamento =2022;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        Double notaDoFilme = 8.1;
+ // media calculada pela jack, paulo e suelen
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse =(String.format( """
+                    Filme top Gun
+                    Filme de aventura com galã dos anos 80
+                    Muito bom!
+                    Ano de lançamento: %d
+                """, anoDeLancamento)) ;
+        System.out.println(sinopse);
+
+
+
+    }
+}
